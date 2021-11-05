edca2719939393a39079b3e652022cef9ae76ba2
ChristianTedesco98 <c.tedesco19@studenti.unisa.it>
2021-11-05 13:08:03 +0100

    Base Bot

--- actions/actions.py
+++ actions/actions.py
@@ -10,22 +10,43 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import json
 
 DATA = {}
+file_loaded = False
 
-class UpdateList(Action):
+class ManageFile():
+
+    def loadData():
+        global DATA, file_loaded 
+        try:
+            file = open("data.json", "r")
+            data_read = file.read()
+            DATA = json.loads(data_read)
+            file.close()
+            print("File Loaded")
+            file_loaded = True
+        except FileNotFoundError:
+            print("Error during file loading")
+            return 
+
+    def writeData():
+        file = open("data.json", "w")
+        json.dump(DATA, file)
+        file.close()
+
+
+class UpdateElement(Action):
 
     def name(self) -> Text:
-        return "update_list"
+        return "update_element"
     
-    def write_file(self):
-        f = open("test.txt", "w")
-        f.write("Something saved")
-        f.close()
-
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        if file_loaded is False:
+            ManageFile.loadData()
         
         element = tracker.get_slot("element")
         quantity = tracker.get_slot("quantity")
@@ -36,13 +57,66 @@ class UpdateList(Action):
         
         if element in DATA:
             old_quantity = DATA[element]
-            new_quantity = old_quantity + quantity
+            new_quantity = str(int(old_quantity) + int(quantity))
             DATA[element] = new_quantity
         else:
             DATA[element] = quantity
         
-        self.write_file()
+        
+        ManageFile.writeData()
         
         dispatcher.utter_message(text="List updated.") 
 
+        return [SlotSet("element", None), SlotSet("quantity", None)]
+
+
+class DisplayList(Action):
+    def name(self) -> Text:
+        return "display_list"
+
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        if file_loaded is False:
+            ManageFile.loadData()
+
+        
+        if DATA:
+            for element in DATA:
+                list_entry = f"{element}:{DATA[element]}\n"
+                dispatcher.utter_message(text=list_entry) 
+        else:
+             dispatcher.utter_message(text="Your list is empty\n")    
+               
         return []
+
+
+class DeleteElement(Action):
+    
+    def name(self) -> Text:
+        return "delete_element"
+    
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        if file_loaded is False:
+            ManageFile.loadData()
+            
+        element = tracker.get_slot("element")
+
+        print("\nElement:",element)
+        
+        if element in DATA:
+            del DATA[element]
+            dispatcher.utter_message(text="Element removed.") 
+        else:
+            dispatcher.utter_message(text="Element not found.") 
+        
+        ManageFile.writeData()
+
+        
+        return [SlotSet("element", None)]
